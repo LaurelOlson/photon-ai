4bdf66e9c65a97a8d7b323e5bf3833a95161e5db
LaurelOlson <laurel.olson@outlook.com>
2016-03-24 14:18:00 -0700

    updated timestamps to remove hardcoding of current time

--- migrations/20160321212055-create-likedphotos.js
+++ migrations/20160321212055-create-likedphotos.js
@@ -20,12 +20,10 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: new Date()
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: new Date()
       }
     });
   },
