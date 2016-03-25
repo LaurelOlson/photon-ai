f5f1277b1b85caa6fee4e9fa31df1c865fd6d24a
LaurelOlson <laurel.olson@outlook.com>
2016-03-24 22:20:39 -0700

    updated

--- seeders/seed.js
+++ seeders/seed.js
@@ -15,11 +15,11 @@ const NUM_PHOTOS = 600;
 
 // SEED PHOTOS
 
-// var readStream = fs.createReadStream('seeders/imglinks.csv');
-// var lineStream = readStream.pipe(split());
-// lineStream.on('data', function(data) {
-//   models.photo.create({ url: data });
-// });
+var readStream = fs.createReadStream('seeders/imglinks.csv');
+var lineStream = readStream.pipe(split());
+lineStream.on('data', function(data) {
+  models.photo.create({ url: data });
+});
 
 // GET PHOTO TAGS & SEED TAGS/PHOTO_TAGS
 
@@ -53,84 +53,83 @@ var visionRequest = {
   ]
 };
 
-models.photo.findAll( { limit: 50 }).then(function(promises) {
-  promises.forEach(function(photo) {
-    seedTag(photo);
-  });
-});
+// models.photo.findAll( { limit: 50 }).then(function(promises) {
+//   promises.forEach(function(photo) {
+//     seedTag(photo);
+//   });
+// });
 
-var download = function(uri, filename, callback) {
-  request.head(uri, function(err, res, body) {
-    if (err) { 
-      return console.error(err);
-    }
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
+// var download = function(uri, filename, callback) {
+//   request.head(uri, function(err, res, body) {
+//     if (err) { 
+//       return console.error(err);
+//     }
+//     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+//   });
+// };
 
-function seedTag(photo) {
-  var file = 'seeders/temp/photo-' + photo.id + '.jpg' 
-  download(photo.url, file, convertToBase64);
+// function seedTag(photo) {
+//   var file = 'seeders/temp/photo-' + photo.id + '.jpg'
+//   download(photo.url, file, convertToBase64);
 
-  function convertToBase64(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    fs.readFile(file, 'base64', sendToGoogleVision);
-  }
+//   function convertToBase64(err, data) {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     fs.readFile(file, 'base64', sendToGoogleVision);
+//   }
 
-  function sendToGoogleVision(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    visionRequest.requests[0].image.content = data;
-    var request_options = {
-      url: 'https://vision.googleapis.com/v1/images:annotate',
-      qs: { key: secret_stuff.vision_key },
-      method: 'POST',
-      json: visionRequest
-    };
-    request(request_options, parseResponse);
-  }
+//   function sendToGoogleVision(err, data) {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     visionRequest.requests[0].image.content = data;
+//     var request_options = {
+//       url: 'https://vision.googleapis.com/v1/images:annotate',
+//       qs: { key: secret_stuff.vision_key },
+//       method: 'POST',
+//       json: visionRequest
+//     };
+//     request(request_options, parseResponse);
+//   }
 
-  function parseResponse(error, response, body) {
-    if (error) {
-      return console.error(error);
-    }
-    var tags = [];
-    var landmarks = body.responses[0].landmarkAnnotations;
-    var labels = body.responses[0].labelAnnotations;
-    if (labels) {
-      labels.forEach(function(label) {
-        tags.push({ name: label.description, type: 'label', score: label.score });
-      });
-    }
-    if (landmarks) {
-      landmarks.forEach(function(landmark) {
-        tags.push({ name: landmark.description, type: 'landmark', score: landmark.score });
-      });
-    }
-    tags.forEach(addTagToDB);
-  }
+//   function parseResponse(error, response, body) {
+//     if (error) {
+//       return console.error(error);
+//     }
+//     var tags = [];
+//     var landmarks = body.responses[0].landmarkAnnotations;
+//     var labels = body.responses[0].labelAnnotations;
+//     var safesearch = body.respones[0].safeSearchAnnotation;
+//     if (labels) {
+//       labels.forEach(function(label) {
+//         if (label.score >= MIN_LABEL_SCORE) {
+//           tags.push({ name: label.description, type: 'label' });
+//         }
+//       });
+//     }
+//     if (landmarks) {
+//       landmarks.forEach(function(landmark) {
+//         tags.push({ name: landmark.description, type: 'landmark' });
+//       });
+//     }
+//     tags.forEach(addTagToDB);
+//   }
 
-  function addTagToDB(vision_tag) {
-    if (vision_tag.score >= MIN_LABEL_SCORE) {
-      models.tag.findOrCreate({ where: { name: vision_tag.name, type: vision_tag.type } }).then(function(promise) {
-        var tag = promise[0];
-        photo.addTag(tag).then(function() {
-          photo.hasTag(tag).then(console.log);
-          tag.hasPhoto(photo).then(console.log);
-        });
-      });
-    }
-  }
-}
+//   function addTagToDB(vision_tag) {
+//     models.tag.findOrCreate({ where: { name: vision_tag.name, type: vision_tag.type } }).then(function(promise) {
+//       var tag = promise[0];
+//       photo.addTag(tag).then(function() {
+//         photo.hasTag(tag).then(console.log);
+//         tag.hasPhoto(photo).then(console.log);
+//       });
+//     });
+//   }
+// }
 
 // SEED USERS, LIKED_PHOTOS, ADDED_PHOTOS
 
-// const NUM_PHOTOS = 600;
-// var i = 0;
-// while (i < 5) { // Create 5 users
+// const NUM < 5) { // Create 5 users
 //   models.user.create({
 //     firstname: faker.name.firstName(),
 //     lastname: faker.name.lastName(),
