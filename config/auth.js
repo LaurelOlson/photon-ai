66039304cb341f4cbd00681012e47d885c31d3e5
LaurelOlson <laurel.olson@outlook.com>
2016-03-26 13:45:12 -0700

    working on oauth

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'facebookAuth': {
+    clientID: '211877392505171',
+    clientSecret: 'f5301b35b0e7575e6b7b7995a8d7eb4f',
+    callbackURL: "https://localhost:3000/auth/facebook/callback"
+  }
+}
---
+++ migrations/20160326131001-recreate-users-table.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      fbook_id: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      fbook_token: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
